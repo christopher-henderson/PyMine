acd37e84b2e1b2e41d1b0e09a4f9ac72294b49a9
Christopher Henderson <chris@chenderson.org>
2014-08-24 10:37:21 -0700

    Update pymineService.py
    Updated minor version number.

--- bin/pymineService.py
+++ bin/pymineService.py
@@ -7,7 +7,7 @@ def main(interactive, args):
     pymine = DaemonHandler()
     if interactive:
         readline.parse_and_bind('set editing-mode vi')
-        print ('PyMine version 1.0')
+        print ('PyMine version v1.1')
         print ('Type "pmhelp", "copyright" or "license" for more information.')
         command = raw_input('>>> ').strip()
         while command != 'exit':
